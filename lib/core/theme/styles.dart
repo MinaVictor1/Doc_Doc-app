c72d1e7984e7482d767d8c592c569dabf8a3a868
Mina Victor <minavictor655@gmail.com>
2024-01-23 21:18:59 +0200

    - added new font styles - added app text weight helper - added login screen - added button and text form field to my app

--- lib/core/theme/styles.dart
+++ lib/core/theme/styles.dart
@@ -1,28 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_omar_ahmed/core/theme/colors.dart';
+import 'package:flutter_advanced_omar_ahmed/core/theme/font_weight_helper.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class TextStyles {
   static TextStyle font24Black700Weight = TextStyle(
     fontSize: 24.sp,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeightHelper.bold,
     color: Colors.black,
   );
   static TextStyle font32BlackBold = TextStyle(
     fontSize: 32.sp,
-    fontWeight: FontWeight.bold,
+    fontWeight: FontWeightHelper.bold,
     color: ColorManger.mainBlue,
   );
 
   static TextStyle font13grayRegular = TextStyle(
     fontSize: 13.sp,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeightHelper.regular,
     color: ColorManger.gray,
   );
 
   static TextStyle font16White600weight = TextStyle(
     fontSize: 16.sp,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeightHelper.semiBold,
     color: Colors.white,
+  );
+
+  static TextStyle font24blue700weight = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeightHelper.bold,
+    color: ColorManger.mainBlue,
+  );
+
+  static TextStyle font14gray400weight = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.regular,
+    color: ColorManger.gray,
+  );
+
+  static TextStyle font14LightGrayregular = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.regular,
+    color: ColorManger.lightGray,
+  );
+  static TextStyle font14DarkBlueMedium = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.medium,
+    color: ColorManger.darkBlue,
+  );
+  static TextStyle font14MainBlueMedium = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.medium,
+    color: ColorManger.mainBlue,
+  );
+
+  static TextStyle font16WhiteSemiBold = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.semiBold,
+    color: Colors.white,
+  );
+  static TextStyle font13DarkBlueMedium = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeightHelper.medium,
+    color: ColorManger.darkBlue,
+  );
+
+  static TextStyle font13BlueSemiBold = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeightHelper.semiBold,
+    color: Colors.blue,
   );
 }
--- lib/feature/login/ui/login.dart
+++ lib/feature/login/ui/login.dart
@@ -1,13 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced_omar_ahmed/core/theme/styles.dart';
+import 'package:flutter_advanced_omar_ahmed/core/widgets/app_text_button.dart';
+import 'package:flutter_advanced_omar_ahmed/core/widgets/app_text_form_fiels.dart';
+import 'package:flutter_advanced_omar_ahmed/feature/login/ui/widgets/already_have_an_account.dart';
+import 'package:flutter_advanced_omar_ahmed/feature/login/ui/widgets/terms_and_conditions.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  Key formKey = GlobalKey<FormState>();
+  bool isObscureText = true;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Welcome Back',
+                  style: TextStyles.font24blue700weight,
+                ),
+                SizedBox(
+                  height: 8.h,
+                ),
+                Text(
+                  'We\'re excited to have you back, can\'t wait to see what you\'ve been up to since you last logged in.',
+                  style: TextStyles.font14gray400weight,
+                ),
+                const SizedBox(
+                  height: 17,
+                ),
+                Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        const AppTextFormField(hintText: 'Email'),
+                        const SizedBox(height: 16),
+                        AppTextFormField(
+                          hintText: 'Password',
+                          isObscureText: isObscureText,
+                          suffixIcon: GestureDetector(
+                            onTap: () => setState(() {
+                              isObscureText = !isObscureText;
+                            }),
+                            child: Icon(isObscureText
+                                ? Icons.visibility_off
+                                : Icons.visibility),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        Align(
+                            alignment: AlignmentDirectional.centerEnd,
+                            child: Text(
+                              'Forget Password',
+                              style: TextStyles.font14MainBlueMedium,
+                            )),
+                        const SizedBox(
+                          height: 41,
+                        ),
+                        AppTextButton(
+                            buttonText: 'Login',
+                            textStyle: TextStyles.font16WhiteSemiBold,
+                            onPressed: () {}),
+                        const SizedBox(height: 32),
+                        const TermsAndConditionsText(),
+                        const SizedBox(height: 24),
+                        const AlreadyHaveAccountText()
+                      ],
+                    ))
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
