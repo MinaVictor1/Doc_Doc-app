d1a74befb78a3d3c8ee203f7a6e9ede77a6a025f
Mina Victor <minavictor655@gmail.com>
2024-03-14 16:07:20 +0200

    - added firebase distribution

--- lib/core/network/dio_factory.dart
+++ lib/core/network/dio_factory.dart
@@ -2,7 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class DioFactory {
-  /// private constructor as I don't want to allow creating an instance of this class
+  //doc comment
+  /// private constructor as I don't want to allow creating an instance of this class use this only for call getDio
   DioFactory._();
 
   static Dio? dio;
